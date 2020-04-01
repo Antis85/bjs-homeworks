2ba8d8aa4c8c6eccb5b38cafe230a75ce6073153
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-04-01 20:50:17 +0300

    HW1.3 done
    Домашка готова, осталось проверить.

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,43 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+for (let argument in arguments) {
+    let valueToCheck = parseInt(arguments[argument]);
+    if (isNaN(valueToCheck)) {
+      switch (+argument) {
+      case 0:
+          return `Параметр <Процентная ставка> содержит неправильное значение <${percent}>`;
+          //break;
+        case 1:
+          return `Параметр <Первоначальный взнос> содержит неправильное значение <${contribution}>`;
+          //break;
+        case 2:
+          return `Параметр <Сумма кредита> содержит неправильное значение <${amount}>`;
+          //break;       
+        //default:
+          //;
+      }
+    }
+  }
+  let percentPurged = parseInt(percent);
+  let contributionPurged = parseInt(contribution);
+  let amountPurged = parseInt(amount);
+  let sumPayment = amountPurged - contributionPurged;
+  let monthCount = (new Date(date).getFullYear() - new Date().getFullYear()) * 12 + new Date(date).getMonth() - new Date().getMonth();
+  let percentPerMonth = percentPurged / 1200;  
+  let paymentPerMonth = sumPayment * (percentPerMonth + percentPerMonth / (Math.pow(1 + percentPerMonth, monthCount) - 1));
+  let totalAmount = +((paymentPerMonth * monthCount).toFixed(2));
+  return totalAmount;
 }
 
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  let greeting;
+  if (name) {
+    greeting = `Привет, мир! Меня зовут ${name}`;
+  } else {
+    greeting = "Привет, мир! Меня зовут Аноним";
+  }
+  console.log(greeting);
+  return greeting;
 }
