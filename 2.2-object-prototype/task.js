de0aaa5c53dc9f563b55978c00c97e4cd344e505
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-04-06 00:43:40 +0300

    HW2.1 done
    Домашка готова, осталось проверить.

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,31 @@
+"use strict";
+
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+  if (animal === undefined) {
+    return null;
+  } else {
+    let animalSound = animal.sound;
+    return animalSound;
+  }
 }
+
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let average = 0;  
+  for (let mark in marks) {
+    average += marks[mark] / marks.length;    
+  }
+  const roundedAverage = Math.round(average);
+  return roundedAverage;
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+function checkBirthday(birthdate) {
+  const now = new Date().getTime();  
+  const birthday = new Date(birthdate).getTime();  
+  const age = now - birthday;
+  const diff = age / (1000 * 24 * 3600 * 366);
+  let verdict = (age > 18) ? true : false;
+  return verdict;
 }
